5844315da38fd3b452800d74a8809f9151b4c580
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2019-12-08 17:34:46 +0100

    blockchain created

---
+++ index.js
@@ -0,0 +1,59 @@
+const SHA256 = require('crypto-js/sha256');
+
+/*
+* create a single block in the blockchain
+* @param index - position of the block in the chain (in the array)
+* @param timestamp - time of block creation 
+* @param data - data that the block holds
+* @param prevHash - hash of the previous block, link to the previous block in the chain
+*/
+class Block {
+
+    constructor(index,  data, prevHash) {
+        this.index = index;
+        this.timestamp = Date.now();
+        this.data = data;
+        this.prevHash = prevHash;
+        this.hash = this.generateHash();
+    }
+
+    // generate a hash for the block
+    generateHash() {
+        return SHA256(this.index + this.timestamp + this.prevHash + JSON.stringify(this.data)).toString();
+    }
+}
+
+
+/*
+* blockchain class
+* create an array of blocks
+*/
+class Blockchain {
+
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    // the first block in the chain is called "genesis block"
+    // has to be created manually
+    createGenesisBlock() {
+        return new Block(0, "Genesis block", "null");
+    }
+
+    getLastBlockInChain() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addNewBlock(newBlockData) {
+        const   newBlockIndex = (this.getLastBlockInChain().index) + 1, 
+                newBlockPrevHash = this.getLastBlockInChain().hash, // link to previously created block
+                newBlock = new Block(newBlockIndex, newBlockData, newBlockPrevHash); // instantiate new block
+        this.chain.push(newBlock); // push the block in the chain        
+    }
+}
+
+
+let blockchain = new Blockchain();
+blockchain.addNewBlock({ "amount": 4, "currency": "USD" });
+blockchain.addNewBlock({ "amount": 15, "currency": "EUR" });
+console.log(blockchain);
