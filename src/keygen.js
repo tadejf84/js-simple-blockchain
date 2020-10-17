4799972f69bb8f11dc931c1aa901ffca1c92e87e
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-17 16:49:51 +0200

    key generator

--- src/keygen.js
+++ src/keygen.js
@@ -4,11 +4,11 @@ const ec = new EC('secp256k1');
 // Generate key pair
 const key = ec.genKeyPair();
 
-// Get public key
+// Generate public key
 const publicKey = key.getPublic('hex');
 console.log(`Your public key is: ${publicKey}`);
 
-// Get private key
+// Generate private key
 const privateKey = key.getPrivate('hex');
 console.log(`Your private key is: ${privateKey}`);
 
